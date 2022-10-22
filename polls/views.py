c8053ab5a1667234c89531f77edf5be441c2657a
junmok <42504239+wnstjd9288@users.noreply.github.com>
2022-10-22 19:46:35 +0900

    firts commit

---
+++ polls/views.py
@@ -0,0 +1,8 @@
+import re
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello,world. polls index");
+
+# Create your views here.
